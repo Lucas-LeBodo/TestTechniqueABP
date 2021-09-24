573a6c9163a915c6873bb9b4771dc7fa7ef1e107
Lucas-LeBodo <lucas.le-bodo@epitech.eu>
2021-09-24 16:17:31 +0200

    update extract  data on xlsx method

--- client/src/components/uploadxlsx.jsx
+++ client/src/components/uploadxlsx.jsx
@@ -24,36 +24,17 @@ const Uploadxlsx = () => {
         var reader = new FileReader();
         reader.onload = (e) => {
 
-            let  infoArray = [];
-            let  fournisseurArray = [];
-            let  marqueArray = [];
-            let  modeleArray = [];
-            let  energieArray = [];
-            let  achatArray = [];
-            let  arriveeArray = [];
-
             var data = e.target.result;
             let readedData = XLSX.read(data, {type: 'binary'});
-            const wsname = readedData.SheetNames[0];
-            const ws = readedData.Sheets[wsname];
-            let xlsxjson = XLSX.utils.sheet_to_json(ws, {header: 1});  
-            for(let i = 1; i < xlsxjson.length; i++){
-                fournisseurArray.push(xlsxjson[i][2]);
-                marqueArray.push(xlsxjson[i][6]);
-                modeleArray.push(xlsxjson[i][8]);
-                energieArray.push(xlsxjson[i][11]);
-                achatArray.push(xlsxjson[i][21]);
-                arriveeArray.push(xlsxjson[i][45]);
-
-            }
-            infoArray.push(fournisseurArray);
-            infoArray.push(marqueArray);
-            infoArray.push(modeleArray);
-            infoArray.push(energieArray);
-            infoArray.push(achatArray);
-            infoArray.push(arriveeArray);
-            setDatacar(infoArray);
-        };
+            readedData.SheetNames.forEach(sheet => {
+                let rowObject = XLSX.utils.sheet_to_json(
+                    readedData.Sheets[sheet]
+                );
+                let jsonObject = rowObject;
+                setDatacar(jsonObject);
+                console.log(jsonObject);
+            });     
+        }
         reader.readAsBinaryString(f);
     };
     return (
